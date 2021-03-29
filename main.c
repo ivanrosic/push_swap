feabea56d1941d0516189007b2ae5ff7cab20904
Ivan Rosic <ivarosic@z4r1p7.42lyon.fr>
2021-03-29 14:03:13 +0200

    all operation are finish

---
+++ main.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ivarosic <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/29 09:33:41 by ivarosic          #+#    #+#             */
+/*   Updated: 2021/03/29 14:01:59 by ivarosic         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int		ft_atoi(const char *str)
+{
+	int i;
+	int np;
+	int nb;
+
+	nb = 0;
+	i = 0;
+	np = 0;
+	while ((str[i] == '\t') || (str[i] == '\n') || (str[i] == '\v')
+			|| (str[i] == '\f') || (str[i] == '\r') || (str[i] == ' '))
+		i++;
+	if ((str[i] == '+') || (str[i] == '-'))
+	{
+		if (str[i] == '-')
+			np++;
+		i++;
+	}
+	while (!((str[i] < '0') || (str[i] > '9')))
+	{
+		nb = nb * 10;
+		nb = nb + str[i] - '0';
+		i++;
+	}
+	if (np % 2 == 1)
+		return (-nb);
+	return (nb);
+}
+
+void ft_fill_struct(char **av, t_stack *s)
+{
+	int i;
+
+	i = 0;
+	while(i < s->size_a)
+	{
+		s->a[i] = ft_atoi(av[i + 1]);
+		i++;
+	}
+}
+
+void	ft_affiche_stack(t_stack *s)
+{
+	int i;
+
+	i = 0;
+	printf("a\tb\n\n");
+	while(i < 8)
+	{
+		if (i < s->size_a)
+			printf("%d\t",s->a[i]);
+		else
+			printf("-\t");
+		if (i < s->size_b)
+			printf("%d\n",s->b[i]);
+		else
+			printf("-\n");
+		i++;
+	}
+			printf("\n");
+}
+
+void ft_init_struct(int ac, char **av, t_stack *s)
+{
+	s->a = malloc(sizeof(int) * 8);
+	s->b = malloc(sizeof(int) * 8);
+	s->size_a = ac - 1;
+	s->size_b = 0;
+	ft_fill_struct(av, s);
+}
+
+int main(int ac, char **av)
+{
+	t_stack *s;
+
+	if(!(s = malloc(sizeof(t_stack))))
+		return(0);
+	ft_init_struct(ac, av, s);
+	s->b[0] = 8;
+	s->b[1] = 6;
+	s->size_b = 2;
+	ft_affiche_stack(s);
+	ft_rra(s);
+	ft_affiche_stack(s);
+}
---
+++ push_swap.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ivarosic <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/29 09:54:25 by ivarosic          #+#    #+#             */
+/*   Updated: 2021/03/29 13:36:23 by ivarosic         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_stack
+{
+	int *a;
+	int size_a;
+	int *b;
+	int size_b;
+}				t_stack;
+
+	void	ft_rrr(t_stack *s);
+	void	ft_rrb(t_stack *s);
+	void	ft_rra(t_stack *s);
+	void	ft_rr(t_stack *s);
+	void	ft_rb(t_stack *s);
+	void	ft_ra(t_stack *s);
+	void	ft_pb(t_stack *s);
+	void	ft_pa(t_stack *s);
+	void	ft_up_a(t_stack *s);
+	void	ft_ss(t_stack *s);
+	void	ft_sb(t_stack *s);
+	void	ft_sa(t_stack *s);
+	void	ft_swap(int *a, int *b);
+	void	ft_affiche_stack(t_stack *s);
+
+#endif
